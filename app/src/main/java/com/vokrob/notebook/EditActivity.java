f86ccb49dc2d82b0b1ccefb7d720c7ede185152c
vokrob <106639488+vokrob@users.noreply.github.com>
2024-10-10 21:51:42 +0700

    Adding Photo Selection for Note

--- app/src/main/java/com/vokrob/notebook/EditActivity.java
+++ app/src/main/java/com/vokrob/notebook/EditActivity.java
@@ -3,17 +3,25 @@ package com.vokrob.notebook;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.vokrob.notebook.db.MyDbManager;
 
 public class EditActivity extends AppCompatActivity {
+    private ImageView imNewImage;
+    private ConstraintLayout imageContainer;
+    private FloatingActionButton fbAddImage;
+    // private ImageButton imEditImage, imDeleteImage;
     private EditText edTitle, edDesc;
     private MyDbManager myDbManager;
 
@@ -40,6 +48,8 @@ public class EditActivity extends AppCompatActivity {
     private void init() {
         edTitle = findViewById(R.id.edTitle);
         edDesc = findViewById(R.id.edDesc);
+        fbAddImage = findViewById(R.id.fbAddImage);
+        imageContainer = findViewById(R.id.imageContainer);
         myDbManager = new MyDbManager(this);
     }
 
@@ -55,7 +65,40 @@ public class EditActivity extends AppCompatActivity {
             myDbManager.closeDb();
         }
     }
+
+    public void onClickDeleteImage(View view) {
+        imageContainer.setVisibility(View.GONE);
+        fbAddImage.setVisibility(View.VISIBLE);
+    }
+
+    public void onClickAddImage(View view) {
+        imageContainer.setVisibility(View.VISIBLE);
+        view.setVisibility(View.GONE);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
